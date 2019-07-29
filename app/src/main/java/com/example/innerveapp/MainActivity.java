2c129c166842927652c2e3bf2bba5af24104d8fc
Jedi18 <targetakhil@gmail.com>
2019-07-29 20:37:01 +0530

    Installed firebase libs and updated gradle (using my email(akhil))

--- app/src/main/java/com/example/innerveapp/MainActivity.java
+++ app/src/main/java/com/example/innerveapp/MainActivity.java
@@ -5,14 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    //private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        //mAuth = FirebaseAuth.getInstance();
     }
 
+    /*
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+    */
     public void changeActivity(View view)
     {
         Intent intent = new Intent(MainActivity.this, SingleUserActivity.class);
