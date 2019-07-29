7359e69fd144beb291734c6d72efcf3792d2877d
Jedi18 <targetakhil@gmail.com>
2019-07-29 19:17:15 +0530

    Changed pr login page

--- app/src/main/java/com/example/innerveapp/MainActivity.java
+++ app/src/main/java/com/example/innerveapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.innerveapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeActivity(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, SingleUserActivity.class);
+        startActivity(intent);
     }
 }
